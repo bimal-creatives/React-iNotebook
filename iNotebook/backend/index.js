9a58a2cc28123520b12397211390cf64a2c0f223
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-06-08 14:08:55 +0530

    changed

--- iNotebook/backend/index.js
+++ iNotebook/backend/index.js
@@ -1,14 +1,4 @@
 const connectToMongo = require('./db');
 
-const express = require('express')
-const app = express()
-const port = 3000
+connectToMongo();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  connectToMongo();
-})
