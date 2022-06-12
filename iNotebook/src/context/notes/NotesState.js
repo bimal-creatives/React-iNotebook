fcd5c51ceb38fb191bdfc45d4efe29585a5b4510
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-06-12 17:09:31 +0530

    68 done

--- iNotebook/src/context/notes/NotesState.js
+++ iNotebook/src/context/notes/NotesState.js
@@ -34,20 +34,9 @@ const NoteState = (props) => {
       body: JSON.stringify({title, description, tag})
     });
 
-    const json = await response.json();
-    console.log(json)
-
-    console.log("Adding a new note")
-    const note = {
-      "_id": "62a353141268cf09f2d6d678",
-      "user": "62a342ca671a8569a0c7a086",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2021-09-03T14:20:09.668Z",
-      "__v": 0
-    };
+    const note = await response.json();
     setNotes(notes.concat(note))
+    
   }
 
 
@@ -64,9 +53,7 @@ const NoteState = (props) => {
         }
       });
       const json = response.json();
-      console.log(json)
-    
-    console.log("Deleting the note with id" + id);
+      
     const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
   }
@@ -84,7 +71,7 @@ const NoteState = (props) => {
       body: JSON.stringify({title, description, tag})
     });
     const json = await response.json();
-    console.log(json)
+   
 
     let newNotes = JSON.parse(JSON.stringify(notes))
 
